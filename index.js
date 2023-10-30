e8b16c921ff001986d761c6a8a5d886fd7ea5ac3
zzpillau <zzpilla@gmail.com>
2023-10-30 21:46:52 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-console.log("Hello, Hexlet!");
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
